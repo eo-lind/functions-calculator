6f87e320c68d82dc1fd1ccd5d88323ae8fc2b975
eo-lind <eo.lind@gmail.com>
2022-02-17 11:05:55 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// add
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+let addResult = add(4, 5)
+console.log(addResult)
+
+// subtract
+const subtract = (firstNumber, secondNumber) => {
+    const sum = firstNumber - secondNumber
+    return sum
+}
+
+let subtractResult = subtract(10, 6)
+console.log(subtractResult)
+
+// divide
+
+const divide = (firstNumber, secondNumber) => {
+    const sum = firstNumber / secondNumber
+    return sum
+}
+
+let divideResult = divide(14, 2)
+console.log(divideResult)
+
+// multiply
+
+const multiply = (firstNumber, secondNumber) => {
+    const sum = firstNumber * secondNumber
+    return sum
+}
+
+let multiplyResult = multiply(30, 2)
+console.log(multiplyResult)
+
+// square
+
+const square = (firstNumber) => {
+    const sum = firstNumber * firstNumber
+    return sum
+}
+
+let squareResult = square(3)
+console.log(squareResult)
+
